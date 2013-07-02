49395dca91467e72a9924c74abd2d1f951a44a04
Dragon Dave <dragon@scraperwiki.com>
2013-07-02 11:04:11 +0000

    command line: number of years

--- emdat.py
+++ emdat.py
@@ -4,15 +4,21 @@ import dl
 import xypath
 import messytables
 import orm
+import sys
+
 """Value: dsID, region, indID, period, value, source, is_number
    DataSet: dsID, last_updated, last_scraped, name
    Indicator: indID, name, units
    """
+try:
+    num_years = int(sys.argv[1])
+except:
+    print "invoke with number of years (1, 10)"
+    raise
 
 url = 'http://cred01.epid.ucl.ac.be:5317/?after=&before=&agg1=iso&agg2=year&dl=true'
 targets = 'no_disasters no_killed no_injured no_affected no_homeless total_affected total_dam'.split(' ')
 names = ["Number of disasters", "People killed in disasters", "People injured in disasters", "People affected by disasters", "People made homeless by disasters", "Total number of people affeccted by disasters", "Total cost of damage done by disasters"]
-num_years = 10
 raw = dl.grab(url)
 m_tables = messytables.any.any_tableset(raw)
 mt, = m_tables.tables
