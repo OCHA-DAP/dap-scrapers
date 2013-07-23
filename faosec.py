f8dd666f68516283f4b549b2b638d8655459093d
Dragon Dave <dragon@scraperwiki.com>
2013-07-23 14:57:28 +0000

    checkpoint: fao

---
+++ faosec.py
@@ -0,0 +1,51 @@
+import requests
+import xypath
+import messytables
+import dl
+import re
+import orm
+
+"""Value: dsID, region, indID, period, value, source, is_number
+   DataSet: dsID, last_updated, last_scraped, name
+   Indicator: indID, name, units
+   """
+
+valuetemplate = {'dsID': 'fao-foodsec',
+                 'indID': 'undernourishment',
+                 'is_number': True}
+
+indicator = {'indID': 'undernourishment',
+             'name': "Prevalence of undernourishment",
+             'units': 'percentage'}
+
+dataset = {'dsID': 'fao-foodsec',
+           'last_updated': None,
+           'last_scraped': orm.now(),
+           'name': 'FAO-Food Security'}
+
+orm.DataSet(**dataset).save()
+orm.Indicator(**indicator).save()
+
+
+def niceyear(s):
+    return s.partition("-")[0]+"P3Y"
+
+def do_file(url="http://bit.ly/14FRxGV"):
+    fh = dl.grab(url)
+    mts = messytables.excel.XLSTableSet(fh)
+    v12 = mts['V12']
+    xy = xypath.Table.from_messy(v12)
+    print "...got"
+    home =  xy.filter("(home)")
+    years = home.fill(xypath.RIGHT)
+    countries = home.fill(xypath.DOWN)
+    for country, year, value in countries.junction(years):
+        values = dict(valuetemplate)
+        values['source'] = url
+        values['region'] = country.value
+        values['period'] = niceyear(year.value)  # TODO
+        values['value'] = value.value
+        orm.Value(**values).save()
+    orm.session.commit()  
+    
+do_file()
---
+++ faostat.py
@@ -0,0 +1,64 @@
+import requests
+import xypath
+import messytables
+import dl
+import re
+import orm
+
+"""Value: dsID, region, indID, period, value, source, is_number
+   DataSet: dsID, last_updated, last_scraped, name
+   Indicator: indID, name, units
+   """
+
+valuetemplate = {'dsID': 'faostat3',
+                 'indID': 'calories_per_person',
+                 'is_number': True}
+
+indicator = {'indID': 'calories_per_person',
+             'name': "Per capita food supply",
+             'units': 'kcal/capita/day'}
+
+dataset = {'dsID': 'faostat3',
+           'last_updated': None,
+           'last_scraped': orm.now(),
+           'name': 'FAOstat'}
+
+orm.DataSet(**dataset).save()
+orm.Indicator(**indicator).save()
+
+def get_zip_urls(basename="http://faostat.fao.org/Portals/_Faostat/Downloads/zip_files/%s"):
+    jsonurl = "http://faostat3.fao.org/wds/rest/bulkdownloads/faostat2/CC/E"
+    ajax = requests.get(jsonurl).json()
+    return [basename % i[2] for i in ajax if '+' not in i[3]]
+
+def do_zip(url):
+    print url
+    fh = dl.grab(url)
+    mt, = list(messytables.zip.ZIPTableSet(fh).tables)
+    fh = None
+    xy = xypath.Table.from_messy(mt)
+    mt = None
+    print "...got"
+    headers = xy.filter(lambda c: c.y==0)
+    country = headers.filter("Country").assert_one()
+    items = headers.filter("Item").assert_one().fill(xypath.DOWN).filter("Grand Total + (Total)")
+    elements = headers.filter("Element").assert_one().fill(xypath.DOWN).filter("Food supply (kcal/capita/day)")
+    filtered_items = items.select_other(lambda a,b: a.y==b.y, elements)
+    
+    years = country.fill(xypath.RIGHT).filter(re.compile("Y\d\d\d\d$"))
+
+    for i in filtered_items:
+        values = dict(valuetemplate)
+        values['source'] = url
+        countrycodecell, = i.junction(country)
+        values['region'] = countrycodecell[2].value
+        year_junction = i.junction(years)
+        for _, period, value in year_junction:
+            values['period'] = period.value.replace("Y","")
+            values['value'] = value.value
+            orm.Value(**values).save()
+    orm.session.commit()  
+    
+
+for i in get_zip_urls():
+    do_zip(i)
