a910583abb6b3d5b9fb0537e8d8758652de2c482
Dragon Dave <dragon@scraperwiki.com>
2013-07-24 14:01:40 +0000

    more updates

--- hdrstats.py
+++ hdrstats.py
@@ -28,9 +28,22 @@ indicator_list = """
    Indicator: indID, name, units
    """
 
+def disasters():
+    baseurl = "http://hdrstats.undp.org/en/tables/displayByRow.cfm"
+    data = {"selectedCountries": "3,103,203,403,503,703,803,903,1103,1203,1303,1403,1503,1603,1703,1803,1903,2003,2103,2303,2403,2503,2603,2703,2903,3003,3103,3203,3303,3403,3503,3603,3803,3903,4003,4103,4203,4303,4403,4503,4703,4803,4903,5003,5103,5203,5303,5403,5503,5603,5703,5803,5903,6003,6103,6203,6303,6603,6703,6803,7103,7203,7303,7403,7503,7703,7903,8203,8303,8403,8503,8603,8803,8903,9003,9103,9203,9303,9403,9503,9603,9803,9903,10003,10103,10203,10303,10403,10503,10603,10703,10803,10903,11003,11103,11203,11303,11403,11503,11603,11703,11803,12103,12203,12303,12403,12503,12603,12703,12903,13003,13203,13303,13403,13503,13603,13703,13903,14003,14103,14203,14303,14403,14503,14803,14903,15003,15103,15503,15603,15703,15803,15903,16003,16103,16203,16303,16403,16603,16703,16903,17103,17203,17303,17503,17603,17803,17903,18003,18103,18203,18303,18403,18603,18703,18803,18903,19003,19103,19203,19303,19403,19503,19603,19703,19903,20003,20103,20203,20403,20503,20603,12003,20703,20803,21003,21103,21203,21303,21403,21603,21703,21803,21903,22003,22103,22203,22303,22403,22503,22603,23003,23103,23203,202,2,102,2602,302,402,602,702,502,902,802,1002,1202,1102,1402,1302,1502,1602,1702,2202,1802,2002,1902,2302,2102,2402,2502,2702,3402,3302,3502,3702,3602,3802,3902,4002,4102,",
+            "selectedIndicators": "98606,",
+            "selectedYears": "1960,1970,1980,1985,1990,1995,2000,2005,2006,2007,2008,2009,2010,2011,2012,",
+            "language": "en",
+            "displayIn": "row"}
+    html = requests.post(baseurl, data=data).content
+    return html, baseurl
 
-def getindicator(ind="100106"):
-    baseurl = 'http://hdrstats.undp.org/en/indicators/display_cf_xls_indicator.cfm?indicator_id=%s&lang=en' % ind
+def getindicator(ind="100106", overridefunction=None):
+    if not overridefunction:
+        html = requests.get(baseurl).content
+        baseurl = 'http://hdrstats.undp.org/en/indicators/display_cf_xls_indicator.cfm?indicator_id=%s&lang=en' % ind
+    else:
+        html, baseurl = overridefunction()
     value = {'dsID': 'HDRStats',
              'indID': "HDR:"+ind,
              'source': baseurl,
@@ -46,7 +59,6 @@ def getindicator(ind="100106"):
                      'units': ''}
     Indicator(**hdi_indicator).save()
     DataSet(**dataset).save()
-    html = requests.get(baseurl).content
     htmlio = StringIO.StringIO(html)
     messy = messytables.html.HTMLTableSet(htmlio)
     table = xypath.Table.from_messy(list(messy.tables)[0])
@@ -88,6 +100,7 @@ def getindicator(ind="100106"):
         newvalue['period'] =i[1].value.strip()
         if newvalue['value'].strip() != '..':
             Value(**newvalue).save()
+        print newvalue
     session.commit()
 
 def get_region(country):
@@ -100,6 +113,8 @@ def get_region(country):
             niceregion, = re.findall("profiles/([^\.]*)\.html", link)
         return niceregion
 
+getindicator("98606", disasters)
+exit()
 for ind in indicator_list:
     print ind
     getindicator(ind)
---
+++ who-athena2.py
@@ -0,0 +1,86 @@
+import StringIO
+import requests
+import xypath
+import messytables
+from messytables import headers_processor
+from hamcrest import equal_to, is_in
+from orm import session, Value, DataSet, Indicator, send
+import orm
+import dl
+import re
+import lxml
+import requests_cache
+
+requests_cache.install_cache("who_cache.db")
+
+"""Value: dsID, region, indID, period, value, source, is_number
+   DataSet: dsID, last_updated, last_scraped, name
+   Indicator: indID, name, units
+   """
+
+secondurl = "http://apps.who.int/gho/athena/data/data-verbose.csv?target=GHO/CM_01,CM_02,CM_03,DEVICES09,DEVICES22,HIV_0000000020,MALARIA001,MDG_0000000001,MDG_0000000003,MDG_0000000005,MDG_0000000005_AGE1519,MDG_0000000006,MDG_0000000006_AGE1519,MDG_0000000007,MDG_0000000010,MDG_0000000011,MDG_0000000013,MDG_0000000014,MDG_0000000017,MDG_0000000020,MDG_0000000023,MDG_0000000025,MDG_0000000025_AGE1519,MDG_0000000026,MDG_0000000029,MDG_0000000031,MDG_0000000033,MDG_0000000034,MH_17,MH_18,MH_6,SA_0000001688,TB_1,TB_tot_newrel,TOBACCO_0000000192,WHOSIS_000001,WHOSIS_000003,WHOSIS_000005,WHOSIS_000011,WHOSIS_000015,WHS10_1,WHS10_8,WHS10_9,WHS2_138,WHS2_162,WHS2_163,WHS2_164,WHS2_165,WHS2_166,WHS2_167,WHS2_168,WHS2_170,WHS2_171,WHS2_172,WHS2_173,WHS2_174,WHS2_3070_all,WHS2_3070_cancer,WHS2_3070_cdd,WHS2_3070_chronic,WHS2_513,WHS2_514,WHS2_515,WHS2_516,WHS2_523,WHS3_40,WHS3_41,WHS3_42,WHS3_43,WHS3_45,WHS3_46,WHS3_47,WHS3_48,WHS3_49,WHS3_50,WHS3_51,WHS3_52,WHS3_53,WHS3_55,WHS3_56,WHS3_57,WHS3_62,WHS4_100,WHS4_106,WHS4_107,WHS4_108,WHS4_111,WHS4_111_AGE1519,WHS4_115,WHS4_117,WHS4_124,WHS4_128,WHS4_129,WHS4_154,WHS4_2530,WHS6_101,WHS6_102,WHS6_116,WHS6_123,WHS6_125,WHS6_127,WHS6_136,WHS6_140,WHS6_144,WHS6_148,WHS6_150,WHS6_517,WHS6_518,WHS6_519,WHS6_520,WHS7_103,WHS7_104,WHS7_105,WHS7_108,WHS7_113,WHS7_120,WHS7_134,WHS7_139,WHS7_143,WHS7_147,WHS7_149,WHS7_156,WHS8_110,WHS9_85,WHS9_86,WHS9_88,WHS9_89,WHS9_90,WHS9_91,WHS9_92,WHS9_93,WHS9_95,WHS9_96,WHS9_97,WHS9_CBR,WHS9_CDR,WHS9_CS&profile=verbose&filter=COUNTRY:ALL;SEX:-;LOCATION:-;WORLDBANKINCOMEGROUP:-;DATASOURCE:-;RESIDENCEAREATYPE:-;WEALTHQUINTILE:-;YEAR:2013;YEAR:2012;YEAR:2011;YEAR:2010;YEAR:2009;YEAR:2008;YEAR:2007;YEAR:2006;YEAR:2005;YEAR:2004;YEAR:2003;YEAR:2002"
+
+thirdurl = "http://apps.who.int/gho/athena/data/data-verbose.csv?target=GHO/WHS9_86,WHS9_88,WHS9_89,WHS9_92,WHS9_96,WHS9_97,WHS9_90&profile=verbose&filter=COUNTRY:*;YEAR:2012;YEAR:2011;YEAR:2010;YEAR:2009;YEAR:2008;YEAR:2007"
+
+dataset = {'dsID':'who-athena-other',
+           'last_updated':None,
+           'last_scraped':orm.now(),
+           'name': 'World Health Organization - Athena'}
+
+DataSet(**dataset).save()    
+
+def geturl(url):
+    value = {'dsID':'who-athena-other',
+             'source':url,
+             'is_number':True}
+    
+    fh = dl.grab(url, timeout=20)
+    messy, = messytables.any.any_tableset(fh).tables
+    table = xypath.Table.from_messy(messy)
+    
+    rows = table.rows() # table has no rows?
+    headers = {
+               'value': table.filter("Display Value").x,
+               'period': table.filter("YEAR (DISPLAY)").x,
+               'indID': table.filter("GHO (CODE)").x,
+               'ind_name': table.filter("GHO (DISPLAY)").x,
+               'region': table.filter("COUNTRY (CODE)").x,
+              }
+
+    def at_header(h):
+        return lambda b: b.x == headers[h]
+
+    def units(s):
+        print repr(s)
+        z = re.findall("(.*)\(([^)]*)\)$",s)
+        if z:
+            assert len(z)==1
+            return z[0]
+        else:
+            return (s,"")
+
+    print headers
+    counter = 0
+    for row in rows:
+        if row.filter(at_header('indID')).value=='GHO':
+            assert row.filter('GHO').y==0, row.filter('GHO').y
+            continue
+        
+        vdict = dict(value)
+        for h in headers:
+            vdict[h] = row.filter(at_header(h)).value
+        u = units(vdict['ind_name'])
+        indicator = {'indID':vdict['indID'],
+                     'units':u[1],
+                     'name':u[0]}
+        del vdict['ind_name']
+        Indicator(**indicator).save()
+        v = Value(**vdict)
+        print vdict
+        if not v.is_blank(): 
+            v.save()
+        counter=counter+1
+    print counter
+    session.commit()
+
+geturl(thirdurl)
