51fc839cfc50c99754b28ad65df24021a5e07e58
Dragon Dave <dragon@scraperwiki.com>
2013-08-28 14:25:08 +0000

    acled numbers only

--- acled.py
+++ acled.py
@@ -3,20 +3,55 @@ import lxml.html
 import dl
 import messytables
 import xypath
+from collections import OrderedDict
+import itertools
+import orm
+
+"""Value: dsID, region, indID, period, value, source, is_number
+   DataSet: dsID, last_updated, last_scraped, name
+   Indicator: indID, name, units
+   """
+
+dataset = {'dsID': 'acled',
+           'last_updated': None,
+           'last_scraped': orm.now()}
+
+orm.DataSet(**dataset).save()
+
+indicator = {'indID': 'PVX040',
+             'name': 'Incidence of Conflict',
+             'units': 'incidents per year'}
+
+orm.Indicator(**indicator).save()
 
 def parsesheet(url):
     rowset = messytables.excel.XLSTableSet(dl.grab(url)).tables[0]
-    for row in rowset:
-        print row
-        return
+    for i, row in enumerate(rowset):
+        if i == 0:
+            headers = [x.value for x in row]
+            continue
+        yield OrderedDict(zip(headers, [x.value for x in row]))
         
-
-
 def geturls(baseurl = 'http://www.acleddata.com/data/types-and-groups/'):
     html = requests.get(baseurl).content
     root = lxml.html.fromstring(html)
     root.make_links_absolute(baseurl)
     return root.xpath("//div[@id='content']//article//a[contains(text(),'xls')]/@href")
 
+def keyfunc(item):
+    return (item['YEAR'], item['COUNTRY'])
+
+events = []
 for url in geturls():
-    parsesheet(url)
+    for row in parsesheet(url):
+        events.append(row)
+sorted_e = sorted(events, key=keyfunc)
+
+for item in itertools.groupby(sorted_e, keyfunc):
+    value_item = {'dsID': 'acled',
+	          'indID': 'PVX040',
+                  'period': item[0][0],
+                  'region': item[0][1], 
+                  'value': len(list(item[1])),
+                  'source': 'http://www.acleddata.com/data/types-and-groups/'}
+    orm.Value(**value_item).save()
