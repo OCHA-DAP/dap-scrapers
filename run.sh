a3a4c4c617f29d233389820d222b83e77ec7b773
Dragon Dave <dragon@scraperwiki.com>
2013-09-12 13:13:45 +0100

    add new things to autorun

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 source ~/libs
-for PYFILE in m49.py emdat.py esa.py faosec.py faostat.py hdrstats.py mdg.py unicef.py unterm.py who-athena.py who-athena2.py worldbank-lendinggroups.py worldbank.py
+for PYFILE in m49.py acled.py echo.py emdat.py esa.py faosec.py faostat.py hdr-disaster.py hdrstats.py mdg.py unicef.py unterm.py weather.py who-athena.py who-athena2.py wikipedia.py worldbank-lendinggroups.py worldbank.py worldaerodata.py
 do
     echo $PYFILE
     python $PYFILE > log1/$PYFILE.txt 2> log2/$PYFILE.txt
 done
+
+# todo: reliefweb-api
+# tood: sanity
