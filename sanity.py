056d4a8d414d78de097a24980362f8cc2dcc90a6
Dragon Dave <dragon@scraperwiki.com>
2013-09-17 09:59:36 +0000

    improve

--- sanity.py
+++ sanity.py
@@ -1,7 +1,10 @@
-#import orm
+import orm
 
-lookup = {}
-lookup['dsIDs'] = '"' + '", "'.join("""
+def getcountrylist():
+    for value in orm.session.query(orm.Value).filter(orm.Value.indID == "m49-name").all():
+        yield value.region
+
+dsIDs = """
 acled
 echo
 emdat
@@ -21,28 +24,27 @@ who-athena
 wikipedia
 worldaerodata
 worldbank-lending-groups
-World Bank""".strip().split('\n')) + '"'
+World Bank""".strip().split('\n')
 
-def getcountrylist():
-    for value in orm.session.query(orm.Value).filter(orm.Value.indID == "m49-name").all():
-        yield value.region
+orm.session.execute("create temporary table dsIDs (dsID);")
+for dsID in dsIDs: 
+    orm.session.execute("insert into dsIDs values ('%s')" % dsID)
 
 def sql(s):
     return orm.session.execute(s).fetchmany(11)
 
 should_be_empty = """
-select distinct dsID from value where source is null; -- Blank source
+select distinct dsID from value where source is null; -- Blank source 
 select distinct dsID from value where source is ''; -- Blank source
 select distinct dsID from value where period like "%P%" and period not like "%/%"; -- Bad recurring
 select distinct dsID from value where period like "%T%"; -- Bad time
 select dsID from dataset where dsID not in (select dsID from value); -- Not minimal list of dsID
 select indID from indicator where indID not in (select indID from value); -- Not minimal list of indID
-select dsID from ({dsIDs}) where dsID not in (select distinct dsID from dataset); -- have run all expected datasets
-select distinct dsID from dataset where dsID not in ({dsIDs}); -- no surprise datasets
+select dsID from dsIDs where dsID not in (select dsID from dataset); -- completeness
 """.strip().split('\n')
 
 for line in should_be_empty:
-    values = sql(line.format(**lookup))
+    values = sql(line)
     number = len(values)
     if number > 10: number = "many"
     if values:
