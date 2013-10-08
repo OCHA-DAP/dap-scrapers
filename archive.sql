326be453f405b3893e9a3b2f96295c6a1f5a8a72
Dragon Dave <dragon@scraperwiki.com>
2013-10-08 10:52:02 +0000

    archive and debug utils

---
+++ archive.sql
@@ -0,0 +1,10 @@
+.mode csv
+.headers on
+.output dataset.csv
+select * from dataset;
+.output value.csv
+select * from value;
+.output indicator.csv
+select * from indicator;
+.output all.sql
+.dump
--- minmax.sql
+++ minmax.sql
@@ -1,4 +1,4 @@
-.output minmax
+.output minmax.csv
 .mode csv
-select min(cast(value as float)), max(cast(value as float)), name, value.indID from value join indicator where value.indID = indicator.indID group by value.indID;
+select min(cast(value as float)), max(cast(value as float)), name, value.indID, value from value join indicator where value.indID = indicator.indID group by value.indID;
 
