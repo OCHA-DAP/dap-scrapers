81730da3a5d707ee223cc9c954880f5437ad8ae3
Dragon Dave <dragon@scraperwiki.com>
2013-10-31 16:03:54 +0000

    run with notification

--- run.sh
+++ run.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 source ~/libs
+STATUS="ok"
+MSG=" failed"
 for PYFILE in m49.py acled.py echo.py emdat.py esa.py faosec.py faostat.py hdr-disaster.py hdrstats.py mdg.py unicef.py unterm.py weather.py who-athena.py who-athena2.py wikipedia.py worldbank-lendinggroups.py worldbank.py worldaerodata.py
 do
     echo $PYFILE
     python $PYFILE > log1/$PYFILE.txt 2> log2/$PYFILE.txt
-    echo EXIT: $?
+    EXIT=$?
+    echo EXIT: $EXIT
+    if [ $EXIT != "0" ]
+        then
+            STATUS="error"
+            MSG=$PYFILE $MSG
+    fi
 done
+
+# todo: upload to CKAN
+
+if [ $STATUS == "0" ]
+    then
+        MSG=
+
+curl https://scraperwiki.com/api/status --data "type=$STATUS&message=$MSG"
+
 
 # todo: reliefweb-api
 # tood: sanity
