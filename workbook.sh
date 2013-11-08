f9aee893eceae763fd2e2b9e725b94ce273af720
Dragon Dave <dragon@scraperwiki.com>
2013-11-08 10:56:53 +0000

    have nice indicator name in workbook

--- workbook.sh
+++ workbook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for country in `echo "select region from value where indID='CG060';" | sqlite3 ocha.db`; do echo ".mode csv
+for country in `echo "select region from value where indID='CG060';" | sqlite3 ocha.db`; do echo $country; echo ".mode csv
 .headers on
 .output csv/$country.csv
-select * from value where region='$country';" | sqlite3 ocha.db; done
+select value.*, indicator.name as indNAME from value, indicator where region='$country' and indicator.indID = value.indID;" | sqlite3 ocha.db; done
