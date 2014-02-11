f67160a18194f0355b7a021f65d050cd95d15568
Dragon Dave <dragon@scraperwiki.com>
2014-02-11 16:07:02 +0000

    kill old db

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 source ~/libs
 STATUS="ok"
 MSG=" failed"
+rm ocha.db
 for PYFILE in m49.py acled.py echo.py emdat.py esa.py faosec.py faostat.py hdr-disaster.py hdrstats.py mdg.py unicef.py unterm.py weather.py wikipedia.py worldbank-lendinggroups.py worldbank.py worldaerodata.py reliefweb-api.py hdr_new.py
 do
     echo $PYFILE
