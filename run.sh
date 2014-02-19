c7c3c5bf13b5f97f6a2624ddc5c5fe5406861f7b
Dragon Dave <dragon@scraperwiki.com>
2014-02-19 17:04:36 +0000

    more informative email

--- run.sh
+++ run.sh
@@ -3,17 +3,17 @@ source ~/libs
 STATUS="ok"
 MSG=" failed"
 rm ocha.db
-for PYFILE in m49.py acled.py echo.py emdat.py esa.py faosec.py faostat.py mdg.py unicef.py unterm.py weather.py wikipedia.py worldbank-lendinggroups.py worldbank.py worldaerodata.py reliefweb-api.py hdr_new.py
+for PYFILE in m49.py acled.py echo.py emdat.py esa.py faosec.py faostat.py mdg.py unicef.py unterm.py weather.py wikipedia.py worldbank-lendinggroups.py worldbank.py worldaerodata.py reliefweb-api.py hdr_new.py hdr-disaster.py
 do
-    echo $PYFILE
     python $PYFILE > log1/$PYFILE.txt 2> log2/$PYFILE.txt
     EXIT=$?
-    echo EXIT: $EXIT
     if [ $EXIT != "0" ]
         then
             STATUS="error"
             MSG=$PYFILE:$MSG
-            echo log2/$PYFILE.txt
+            echo $PYFILE $EXIT
+            cat log2/$PYFILE.txt
+            echo
     fi
 done
 
